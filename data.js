4b01e4345b02d8e0c6be3329baee943fa24beacf
= <=>
2022-03-04 08:52:08 +0700

    Init

---
+++ data.js
@@ -0,0 +1,58 @@
+let data = [{
+    id: 1,
+    Nama: "Zinedine Ziddan Fahdlevy",
+    Kelas: "XII SIJA 1",
+    Jurusan: "SIJA",
+    MTK: 80,
+    BINDO: 90,
+    BING: 75,
+    tanggallahir: new Date(2003, 10, 12)
+}, {
+    id: 2,
+    Nama: "Lazzian",
+    Kelas: "XII SIJA 3",
+    Jurusan: "TFLM",
+    MTK: 80,
+    BINDO: 80,
+    BING: 95,
+    tanggallahir: new Date(2003, 1, 22)
+}, {
+    id: 3,
+    Nama: "Doddy",
+    Kelas: "XII SIJA 4",
+    Jurusan: "TEDK",
+    MTK: 90,
+    BINDO: 87,
+    BING: 90,
+    tanggallahir: new Date(2003, 3, 23)
+
+}, {
+    id: 4,
+    Nama: "Dela",
+    Kelas: "XII SIJA 3",
+    Jurusan: "SIJA",
+    MTK: 95,
+    BINDO: 75,
+    BING: 50,
+    tanggallahir: new Date(2003, 11, 1)
+
+}, {
+    id: 5,
+    Nama: "Yusuf",
+    Kelas: "XII SIJA 5",
+    Jurusan: "TEDK",
+    MTK: 100,
+    BINDO: 100,
+    BING: 80,
+    tanggallahir: new Date(2003, 1, 1)
+}, {
+    id: 6,
+    Nama: "Udin",
+    Kelas: "XII SIJA 3",
+    Jurusan: "TGB",
+    MTK: 50,
+    BINDO: 100,
+    BING: 65,
+    tanggallahir: new Date(2003, 9, 10)
+
+}]
